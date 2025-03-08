24d3a469a31ffd4af45fb0f2b88a8c53cb992b12
Dazed <psmite02@gmail.com>
2025-03-08 11:30:37 +0100

    Mobile changes

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const hamburger = document.getElementById("hamburger");
-    const overlayMenu = document.getElementById("overlay-menu");
-    const body = document.body;
-  
-    hamburger.addEventListener("click", function() {
-      const isMenuVisible = overlayMenu.classList.contains("show");
-      overlayMenu.classList.toggle("show");
-      body.style.overflow = isMenuVisible ? "auto" : "hidden"; // Toggle body overflow
-    });
+  const hamburger = document.getElementById("hamburger");
+  const overlayMenu = document.getElementById("overlay-menu");
+  const body = document.body;
+
+  hamburger.addEventListener("click", function() {
+    const isMenuVisible = overlayMenu.classList.contains("show");
+    overlayMenu.classList.toggle("show");
+    body.style.overflow = isMenuVisible ? "auto" : "hidden"; // Toggle body overflow
   });
+});
