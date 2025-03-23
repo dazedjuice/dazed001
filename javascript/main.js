a516618fda8aa6803b1fc32e1eef27bd604a9403
Dazed <psmite02@gmail.com>
2025-03-23 18:40:00 +0100

    Changes

--- javascript/main.js
+++ javascript/main.js
@@ -126,7 +126,21 @@ Draggable.create(cat, {
   inertia: true // Optional: Add inertia to the dragging
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const backToTopButton = document.getElementById("backToTop");
 
+  window.addEventListener("scroll", function () {
+      if (window.scrollY > 300) {
+          backToTopButton.style.display = "block";
+      } else {
+          backToTopButton.style.display = "none";
+      }
+  });
+
+  backToTopButton.addEventListener("click", function () {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+});
 
 
 
